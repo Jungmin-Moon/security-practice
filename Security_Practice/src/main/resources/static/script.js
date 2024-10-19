5f2ff6e9dfdb6b4b899eda58446065c70ed03ace
Jungmin Moon <jmmoon96@gmail.com>
2024-10-19 01:48:50 -0400

    JS works now, can move back to coding the Java side again.

--- Security_Practice/src/main/resources/static/script.js
+++ Security_Practice/src/main/resources/static/script.js
@@ -7,4 +7,28 @@ function toggleDepositForm() {
 	else {
 		depositDiv.style.display = "none";
 	}
-};
+}
+
+
+function toggleWithdrawForm() {
+	var withdrawDiv = document.getElementById("withdrawForm");
+	
+	if (withdrawDiv.style.display == "none") {
+		withdrawDiv.style.display = "block";
+	} 
+	else {
+		withdrawDiv.style.display = "none";
+	}
+}
+
+
+function toggleTransferForm() {
+	var transferDiv = document.getElementById("transferForm");
+	
+	if (transferDiv.style.display == "none") {
+		transferDiv.style.display = "block";
+	} 
+	else {
+		transferDiv.style.display = "none";
+	}
+}
