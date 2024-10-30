85d6e69b337fb75daa4fd3091ec32c3291d1e4e5
Jungmin Moon <jmmoon96@gmail.com>
2024-10-30 03:03:44 -0400

    model add attribute line added for registration page

--- Security_Practice/src/main/java/com/example/demo/controllers/RegisterController.java
+++ Security_Practice/src/main/java/com/example/demo/controllers/RegisterController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,12 +28,13 @@ public class RegisterController {
 	}
 	
 	@PostMapping()
-	public String registerPost(User user) {
+	public String registerPost(User user, Model model) {
 		
 		if (userService.createUser(user) == false) {
 			return "redirect:/home";
 		} else {
 			String message = "The username " + user.getUsername() + " was already taken. Please use a different one.";
+			model.addAttribute("error message",message);		
 			return "register";
 		}
 		
