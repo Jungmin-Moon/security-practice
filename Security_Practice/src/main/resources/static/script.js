fb4d3c53ae106845a75b9e803ce8d90e73848b94
Jungmin Moon <jmmoon96@gmail.com>
2024-11-11 23:39:43 -0500

    Finished up simple CSS for transactions page and cleared the values of
    input boxes when they are not shown so users don't accidentally submit
    
    an incorrect amount.

--- Security_Practice/src/main/resources/static/script.js
+++ Security_Practice/src/main/resources/static/script.js
@@ -2,10 +2,16 @@ var depositDiv = document.getElementById("depositForm");
 var withdrawDiv = document.getElementById("withdrawForm");
 var transferDiv = document.getElementById("transferForm");
 
+var depositAmount = document.getElementById("depositAmount");
+var withdrawAmount = document.getElementById("withdrawAmount");
+var transferAmount = document.getElementById("transferAmount");
+var transferTarget = document.getElementById("transferTarget");
+
 function toggleDepositForm() {
 	
 	if (depositDiv.style.display == "none" && withdrawDiv.style.display == "none" && transferDiv.style.display == "none") {
 		depositDiv.style.display = "block";
+		depositAmount.value = "";
 	} 
 	else {
 		depositDiv.style.display = "none";
@@ -17,6 +23,7 @@ function toggleWithdrawForm() {
 	
 	if (withdrawDiv.style.display == "none" && depositDiv.style.display == "none" && transferDiv.style.display == "none") {
 		withdrawDiv.style.display = "block";
+		withdrawAmount.value = "";
 	} 
 	else {
 		withdrawDiv.style.display = "none";
@@ -28,6 +35,8 @@ function toggleTransferForm() {
 	
 	if (transferDiv.style.display == "none" && withdrawDiv.style.display == "none" && depositDiv.style.display == "none") {
 		transferDiv.style.display = "block";
+		transferAmount.value = "";
+		transferTarget.value = "";
 	} 
 	else {
 		transferDiv.style.display = "none";
